da97d433f2f2b5ccfab5c81712a1fac50b5aef01
oguzhan <oguzhanselamoglu@outlook.com>
2022-12-19 09:26:47 +0300

    Azure Queue Service added

---
+++ StorageService/Services/QueueService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace StorageService.Services
+{
+    public class QueueService
+    {
+        private readonly QueueClient _queueClient;
+
+        public QueueService(string queueName)
+        {
+            _queueClient = new QueueClient(ConnectionStrings.AzureStorageConnectionString, queueName);
+            _queueClient.CreateIfNotExists();
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await _queueClient.SendMessageAsync(message);
+        }
+        public async Task<QueueMessage?> RetrieveMessageAsync()
+        {
+            QueueProperties properties = await _queueClient.GetPropertiesAsync();
+            if (properties.ApproximateMessagesCount > 0)
+            {
+                var messages = await _queueClient.ReceiveMessageAsync();
+                if (messages.Value != null)
+                {
+                    return messages.Value;
+                }
+
+            }
+            return null;
+        }
+    }
+}
+
